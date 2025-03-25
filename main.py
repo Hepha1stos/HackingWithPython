572604bf8738107041e05426151500ed8814dcec
Hepha1stos <moritz.brunner@gmx.de>
2025-03-25 08:36:10 +0100

    implemented new routes

--- main.py
+++ main.py
@@ -14,5 +14,17 @@ def main():
   name = request.args.get("name","Test")
   return render_template("main.html", vars = vars, name=name)
 
+@app.route("/")
+def homepage():
+    return render_template('home.html')
+
+@app.route("/tickets")
+def tickets_page():
+    items= [{"id":1,"priority":2,"username":"Mark","title":"something broken"},
+            {"id":2,"priority":4,"username":"Luke","title":"something working"},
+            {"id":3,"priority":1,"username":"Thomas","title":"something waiting"}
+            ]
+    return render_template("tickets.html",items=items)
+
 if __name__ == "__main__":
   app.run(debug=True)
