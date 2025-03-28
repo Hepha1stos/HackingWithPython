d59014b07a98cbd9326a207980ad4e3072bf6b6e
Hepha1stos <moritz.brunner@gmx.de>
2025-03-28 16:47:44 +0100

    added exception handling

--- src/auth_routes.py
+++ src/auth_routes.py
@@ -1,4 +1,6 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, request, Blueprint,flash
+
+from src.db import mysql
 
 
 auth_routes = Blueprint("auth_routes", __name__)
@@ -7,10 +9,12 @@ auth_routes = Blueprint("auth_routes", __name__)
 def user_login():
   if request.method == "POST":
     print("Post")
-    
+
+   
     username = request.form.get("username")
     password = request.form.get("password")
-    
+  
+
     print(username,password)
     return render_template("login.html")
   return render_template("login.html")
@@ -20,13 +24,24 @@ def user_login():
 def user_register():
   if request.method == "POST":
     print("Post")
-    
-    username = request.form.get("username")
-    email = request.form.get("email")
-    password1 = request.form.get("password1")
-    password2 = request.form.get("password2")
-    
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    try:
+      username = request.form.get("username")
+      email = request.form.get("email")
+      password1 = request.form.get("password1")
+      password2 = request.form.get("password2")
+    except Exception as e:
+      conn.rollback()
+    finally:
+      cursor.close()
+      conn.close()
+  
     print(username,email,password1,password2)
     
+    cursor.execute(f"INSERT INTO users (username, email, password) VALUES ('{username}','{email}','{password2}')")
+    conn.commit()
+
+
     return render_template("register.html")
   return render_template("register.html")
